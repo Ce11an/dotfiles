afaae0582e69beae319ac5b9f3d8b33a2d6bb0f0
Cellan Hall <hallcellan@gmail.com>
2024-10-10 19:31:12 +0100

    Add `telescope-undo`

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -599,6 +599,31 @@ require('lazy').setup({
     main = 'ibl',
     opts = {},
   },
+  {
+    'debugloop/telescope-undo.nvim',
+    dependencies = {
+      {
+        'nvim-telescope/telescope.nvim',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+      },
+    },
+    keys = {
+      {
+        '<leader>u',
+        '<cmd>Telescope undo<cr>',
+        desc = 'undo history',
+      },
+    },
+    opts = {
+      extensions = {
+        undo = {},
+      },
+    },
+    config = function(_, opts)
+      require('telescope').setup(opts)
+      require('telescope').load_extension 'undo'
+    end,
+  },
 
   require('surrealql').setup(),
 }, {
